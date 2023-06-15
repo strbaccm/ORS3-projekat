f3ca8b41258081a4dbb5ab624d99229799b8bf41
strbaccm <110546728+strbaccm@users.noreply.github.com>
2023-06-15 10:02:23 +0200

    Update Process.java

--- projekat_ORS3/src/kernel/Process.java
+++ projekat_ORS3/src/kernel/Process.java
@@ -3,7 +3,6 @@ package kernel;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.Scanner;
 import memory.Memory;
