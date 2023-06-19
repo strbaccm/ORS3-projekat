43ebece9b602e4484f103d8a14c575d34124a11d
strbaccm <110546728+strbaccm@users.noreply.github.com>
2023-06-19 16:47:10 +0200

    Update FileSystem.java

--- projekat_ORS3/src/fileSystem/FileSystem.java
+++ projekat_ORS3/src/fileSystem/FileSystem.java
@@ -1,6 +1,7 @@
 package fileSystem;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -9,6 +10,8 @@ import java.nio.file.Paths;
 import javafx.scene.control.TreeItem;
 import memory.StoredFile;
 import shell.Shell;
+import kernel.Process;
+import asembler.Operations;
 
 public class FileSystem {
 	private static File rootFolder;
@@ -68,6 +71,55 @@ public class FileSystem {
 				if (file.getValue().getName().equals(directory) && file.getValue().isDirectory())
 					currentFolder = file.getValue();
 			}
+	}
+
+	public static void makeDirectory(String directory) {
+		File folder = new File(currentFolder.getAbsolutePath() + "\\" + directory);
+		if(!folder.exists()) {
+			folder.mkdir();
+		}
+	}
+
+	public static void deleteDirectory(String directory) {
+		for(TreeItem<File> file : Shell.tree.getTreeItem().getChildren()) {
+			if( file.getValue().getName().equals(directory) &&
+					file.getValue().isDirectory())
+				file.getValue().delete();
+		}
+	}
+
+	public static void renameDirectory(String oldName, String newName) {
+		for(TreeItem<File> file: Shell.tree.getTreeItem().getChildren()) {
+			if(file.getValue().getName().equals(oldName) &&
+					file.getValue().isDirectory())
+				file.getValue().renameTo(new File(currentFolder.getAbsolutePath() + "\\" + newName));
+		}
+	}
+
+	public static void createFile(Process p) {
+		String name = p.getName().substring(0, p.getName().indexOf('.')) + "_output";
+		File newFile = new File(p.getPath().getParent() + "\\" + name + ".txt");
+		try {
+			newFile.createNewFile();
+			FileWriter fw = new FileWriter(newFile);
+			fw.write("Rezultat izvrsavanja: " + Operations.R4.value);
+			fw.close();
+		} catch (IOException e) {
+			System.out.println("Error while creating file");
+		}
+	}
+
+	public static void deleteFile(String name) {
+		for(TreeItem<File> file: Shell.tree.getTreeItem().getChildren()) {
+			if (file.getValue().getName().equals(name) && !file.getValue().isDirectory())
+				file.getValue().delete();
+			if(Shell.disk.contains(name))
+				Shell.disk.deleteFile(Shell.disk.getFile(name));
+		}
+	}
+
+	public File getCurrentFolder() {
+		return currentFolder;
 	}
 	
 }
