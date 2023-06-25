dca5e91a84dd9aa5bb3c78b6ec16c94633580870
vujicivana <119333651+vujicivana@users.noreply.github.com>
2023-06-25 15:26:40 +0200

    Update RAM.java

--- projekat_ORS3/src/memory/RAM.java
+++ projekat_ORS3/src/memory/RAM.java
@@ -82,9 +82,10 @@ public class RAM {
 	public static void printRAM() {
 		System.out.println("~~~~~~~~~~ RAM ~~~~~~~~~~");
 		for (int i = 0; i < size; i++) {
-			if (i % 32 == 0)
+			if (i % 27 == 0 && i != 0)
 				System.out.println();
 			System.out.print(ram[i] + "\t");
 		}
+		System.out.println();
 	}
 }
